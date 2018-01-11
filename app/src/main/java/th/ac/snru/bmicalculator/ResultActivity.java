3df88409cd85dbde035580a7235c5fcae0869872
smeesathit <smeesathit.gmail.com>
2018-01-11 22:46:02 +0700

    ResultActivity - compute and show bmi.

--- app/src/main/java/th/ac/snru/bmicalculator/ResultActivity.java
+++ app/src/main/java/th/ac/snru/bmicalculator/ResultActivity.java
@@ -1,8 +1,10 @@
 package th.ac.snru.bmicalculator;
 
 import android.content.Intent;
+import java.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
@@ -16,9 +18,21 @@ public class ResultActivity extends AppCompatActivity {
         String wText = intent.getStringExtra("weight");
         String hText = intent.getStringExtra("hight");
 
-        String message = "Weight = " + wText + "  " + "Hight = "+ hText;
-        Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+       // String message = "Weight = " + wText + "  " + "Hight = "+ hText;
+       // Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+        double w;
+        double h;
+        double bmi;
 
+        w = Double.parseDouble(wText);
+        h = Double.parseDouble(hText);
 
+        h = h / 100.0;
+        bmi = w / (h * h);
+
+        TextView bmiValue = findViewById(R.id.bmiValue);
+        DecimalFormat df = new DecimalFormat("0.00");
+        //bmiValue.setText(df.format(bmi));
+        bmiValue.setText(String.valueOf(bmi));
     } // End onCreate
 } // End ResultActivity
