aec641c10c56fddb7a7490642029c9959db8dd6a
smeesathit <smeesathit.gmail.com>
2018-01-14 16:39:06 +0700

    Application refined - add Back button.

--- app/src/main/java/th/ac/snru/bmicalculator/ResultActivity.java
+++ app/src/main/java/th/ac/snru/bmicalculator/ResultActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import java.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +32,14 @@ public class ResultActivity extends AppCompatActivity {
         h = h / 100.0;
         bmi = w / (h * h);
 
-        TextView bmiValue = findViewById(R.id.bmiValue);
+
+        TextView bmiValue = (TextView) findViewById(R.id.bmiValue);
         // bmiValue.setText(String.valueOf(bmi));
         DecimalFormat df = new DecimalFormat("0.00");
         bmiValue.setText(df.format(bmi));
         // bmiValue.setText(String.format("%.2f", bmi));
-        TextView bmiHeader = findViewById(R.id.bmiHeader);
-        TextView bmiDescription = findViewById(R.id.bmiDescription);
+        TextView bmiHeader = (TextView) findViewById(R.id.bmiHeader);
+        TextView bmiDescription = (TextView) findViewById(R.id.bmiDescription);
 
         if (bmi < 18.5){
             bmiHeader.setText(R.string.underWeightHeader);
@@ -49,5 +52,13 @@ public class ResultActivity extends AppCompatActivity {
             bmiDescription.setText(R.string.overWeight);
         }
 
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(this);
     } // End onCreate
+
+
+    @Override
+    public void onClick(View v) {
+        finish();
+    } // End onClick
 } // End ResultActivity
